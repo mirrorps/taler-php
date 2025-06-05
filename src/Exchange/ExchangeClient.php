1edcfff162e686f912e4b7b37d57437b8a11edd4
mirrorps <mirrorps.foss@gmail.com>
2025-06-05 18:35:11 +0300

    Added ExchangeClient

--- src/Exchange/ExchangeClient.php
+++ src/Exchange/ExchangeClient.php
@@ -55,17 +55,31 @@ class ExchangeClient
     }
 
     /**
-     * Retrieve information about deposits
-     * 
+     * @param $H_WIRE string (the hash of the merchant’s payment details)
+     * @param $MERCHANT_PUB string (the merchant’s public key (EdDSA))
+     * @param $H_CONTRACT_TERMS string (the hash of the contract terms that were paid)
+     * @param $COIN_PUB string (the public key of the coin used for the payment)
+     *
+     * Query params:
+     * @param $merchant_sig string (EdDSA signature of the merchant made with purpose TALER_SIGNATURE_MERCHANT_TRACK_TRANSACTION over a TALER_DepositTrackPS, affirming that it is really the merchant who requires obtaining the wire transfer identifier)
+     * @param $timeout_ms int|float [Optional] (If specified, the exchange will wait up to NUMBER milliseconds for completion of a deposit operation before sending the HTTP response)
+     * @param $lpt int [Optional] (Specifies what status change we are long-polling for. Use 1 to wait for the a 202 state where kyc_ok is false or a 200 OK response. 2 to wait exclusively for a 200 OK response)
+     *
      * @return array<string, mixed>|null
+     *
+     * https://docs.taler.net/core/api-exchange.html#get--deposits-$H_WIRE-$MERCHANT_PUB-$H_CONTRACT_TERMS-$COIN_PUB
      */
     public function getDeposits(
         string $H_WIRE,
         string $MERCHANT_PUB,
         string $H_CONTRACT_TERMS,
-        string $COIN_PUB): ?array
+        string $COIN_PUB,
+        string $merchant_sig,
+        ?string $timeout_ms = null,
+        ?int $lpt = null,
+    ): ?array
     {
-        $response = $this->client->request('GET', "deposits/{$H_WIRE}/{$MERCHANT_PUB}/{$H_CONTRACT_TERMS}/{$COIN_PUB}", []);
+        $response = $this->client->request('GET', "deposits/{$H_WIRE}/{$MERCHANT_PUB}/{$H_CONTRACT_TERMS}/{$COIN_PUB}?merchant_sig={$merchant_sig}&timeout_ms={$timeout_ms}&lpt={$lpt}", []);
         return json_decode((string)$response->getBody(), true) ?? null;
     }
 }
