446bca900cdabc878c897d7d269c31485f25e97b
mirrorps <mirrorps.foss@gmail.com>
2025-06-05 19:04:15 +0300

    Added ExchangeClient DTOs:  - TrackTransactionAcceptedResponse  - TrackTransactionResponse

---
+++ src/Api/Exchange/Dto/TrackTransactionResponse.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Taler\Api\Exchange\Dto;
+
+/**
+ * DTO for the track transaction response from the exchange API
+ * 
+ * @see https://docs.taler.net/core/api-exchange.html#get--deposits-$H_WIRE-$MERCHANT_PUB-$H_CONTRACT_TERMS-$COIN_PUB
+ */
+class TrackTransactionResponse
+{
+    /**
+     * @param string $wtid Raw wire transfer identifier of the deposit (Base32)
+     * @param string $execution_time When was the wire transfer given to the bank (Timestamp)
+     * @param string $coin_contribution The contribution of this coin to the total (without fees)
+     * @param string $exchange_sig Binary-only Signature with purpose TALER_SIGNATURE_EXCHANGE_CONFIRM_WIRE over a TALER_ConfirmWirePS
+     * @param string $exchange_pub Public EdDSA key of the exchange used to generate the signature
+     */
+    public function __construct(
+        public readonly string $wtid,
+        public readonly string $execution_time,
+        public readonly string $coin_contribution,
+        public readonly string $exchange_sig,
+        public readonly string $exchange_pub,
+    ) {
+    }
+
+    /**
+     * Creates a new instance from an array of data
+     *
+     * @param array{
+     *     wtid: string,
+     *     execution_time: string,
+     *     coin_contribution: string,
+     *     exchange_sig: string,
+     *     exchange_pub: string
+     * } $data
+     */
+    public static function fromArray(array $data): self
+    {
+        return new self(
+            wtid: $data['wtid'],
+            execution_time: $data['execution_time'],
+            coin_contribution: $data['coin_contribution'],
+            exchange_sig: $data['exchange_sig'],
+            exchange_pub: $data['exchange_pub']
+        );
+    }
+} 
