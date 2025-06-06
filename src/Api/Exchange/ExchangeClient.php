bf7d8517ce6c99f65c97129bffeb820108a61b57
s.stoev <miro@protexting.com>
2025-06-06 10:59:41 +0300

    Update ExchangeClient.php to support async operations

--- src/Api/Exchange/ExchangeClient.php
+++ src/Api/Exchange/ExchangeClient.php
@@ -22,6 +22,8 @@ class ExchangeClient
     /**
      * @param array<string, string> $headers Optional request headers
      * @return array<string, mixed>|null
+     * @throws TalerException
+     * @throws \Throwable
      */
     public function getConfig(array $headers = []): ?array
     {
@@ -32,7 +34,20 @@ class ExchangeClient
 
     /**
      * @param array<string, string> $headers Optional request headers
+     * @return mixed
+     * @throws TalerException
+     * @throws \Throwable
+     */
+    public function getConfigAsync(array $headers = []): mixed
+    {
+        return $this->client->requestAsync('GET', 'config', $headers);
+    }
+
+    /**
+     * @param array<string, string> $headers Optional request headers
      * @return array<string, mixed>|null
+     * @throws TalerException
+     * @throws \Throwable
      */
     public function getKeys(array $headers = []): ?array
     {
@@ -42,10 +57,21 @@ class ExchangeClient
     }
 
     /**
-     * Retrieve the exchange's management keys
-     * 
+     * @param array<string, string> $headers Optional request headers
+     * @return mixed
+     * @throws TalerException
+     * @throws \Throwable
+     */
+    public function getKeysAsync(array $headers = []): mixed
+    {
+        return $this->client->requestAsync('GET', 'keys', $headers);
+    }
+
+    /**
      * @param array<string, string> $headers Optional request headers
      * @return array<string, mixed>|null
+     * @throws TalerException
+     * @throws \Throwable
      */
     public function getManagementKeys(array $headers = []): ?array
     {
@@ -55,12 +81,23 @@ class ExchangeClient
     }
 
     /**
-     * Retrieve details about a specific wire transfer
-     * 
+     * @param array<string, string> $headers Optional request headers
+     * @return mixed
+     * @throws TalerException
+     * @throws \Throwable
+     */
+    public function getManagementKeysAsync(array $headers = []): mixed
+    {
+        return $this->client->requestAsync('GET', 'management/keys', $headers);
+    }
+
+    /**
      * @param string $wtid The wire transfer identifier
      * @param array<string, string> $headers Optional request headers
      * @return TrackTransferResponse|array<string, mixed>
-     * 
+     *
+     * @throws TalerException
+     * @throws \Throwable
      * @see https://docs.taler.net/core/api-exchange.html#get--transfers-$WTID
      */
     public function getTransfer(string $wtid, array $headers = []): TrackTransferResponse|array
@@ -72,6 +109,20 @@ class ExchangeClient
         }
 
         return $this->handleTransferResponse($response);
+    }
+
+    /**
+     * @param string $wtid The wire transfer identifier
+     * @param array<string, string> $headers Optional request headers
+     * @return mixed
+     *
+     * @throws TalerException
+     * @throws \Throwable
+     * @see https://docs.taler.net/core/api-exchange.html#get--transfers-$WTID
+     */
+    public function getTransferAsync(string $wtid, array $headers = []): mixed
+    {
+        return $this->client->requestAsync('GET', "transfers/{$wtid}", $headers);
     }
 
     /**
@@ -93,9 +144,9 @@ class ExchangeClient
      * @param string $MERCHANT_PUB (the merchant's public key (EdDSA))
      * @param string $H_CONTRACT_TERMS (the hash of the contract terms that were paid)
      * @param string $COIN_PUB (the public key of the coin used for the payment)
-     * 
+     *
      * Query params:
-     * 
+     *
      * @param string $merchant_sig (EdDSA signature of the merchant made with purpose TALER_SIGNATURE_MERCHANT_TRACK_TRANSACTION over a TALER_DepositTrackPS, affirming that it is really the merchant who requires obtaining the wire transfer identifier)
      * @param string|null $timeout_ms [Optional] (If specified, the exchange will wait up to NUMBER milliseconds for completion of a deposit operation before sending the HTTP response)
      * @param int|null $lpt [Optional] (Specifies what status change we are long-polling for. Use 1 to wait for the a 202 state where kyc_ok is false or a 200 OK response. 2 to wait exclusively for a 200 OK response)
@@ -103,6 +154,8 @@ class ExchangeClient
      *
      * @return TrackTransactionResponse|TrackTransactionAcceptedResponse|ErrorDetail|array<string, mixed>
      *
+     * @throws TalerException
+     * @throws \Throwable
      * @see https://docs.taler.net/core/api-exchange.html#get--deposits-$H_WIRE-$MERCHANT_PUB-$H_CONTRACT_TERMS-$COIN_PUB
      */
     public function getDeposits(
@@ -123,6 +176,39 @@ class ExchangeClient
         }
 
         return $this->handleDepositsResponse($response);
+    }
+
+    /**
+     * @param string $H_WIRE (the hash of the merchant's payment details)
+     * @param string $MERCHANT_PUB (the merchant's public key (EdDSA))
+     * @param string $H_CONTRACT_TERMS (the hash of the contract terms that were paid)
+     * @param string $COIN_PUB (the public key of the coin used for the payment)
+     *
+     * Query params:
+     *
+     * @param string $merchant_sig (EdDSA signature of the merchant made with purpose TALER_SIGNATURE_MERCHANT_TRACK_TRANSACTION over a TALER_DepositTrackPS, affirming that it is really the merchant who requires obtaining the wire transfer identifier)
+     * @param string|null $timeout_ms [Optional] (If specified, the exchange will wait up to NUMBER milliseconds for completion of a deposit operation before sending the HTTP response)
+     * @param int|null $lpt [Optional] (Specifies what status change we are long-polling for. Use 1 to wait for the a 202 state where kyc_ok is false or a 200 OK response. 2 to wait exclusively for a 200 OK response)
+     * @param array<string, string> $headers Optional request headers
+     *
+     * @return mixed
+     *
+     * @throws TalerException
+     * @throws \Throwable
+     * @see https://docs.taler.net/core/api-exchange.html#get--deposits-$H_WIRE-$MERCHANT_PUB-$H_CONTRACT_TERMS-$COIN_PUB
+     */
+    public function getDepositsAsync(
+        string $H_WIRE,
+        string $MERCHANT_PUB,
+        string $H_CONTRACT_TERMS,
+        string $COIN_PUB,
+        string $merchant_sig,
+        ?string $timeout_ms = null,
+        ?int $lpt = null,
+        array $headers = []
+    ): mixed
+    {
+        return $this->client->requestAsync('GET', "deposits/{$H_WIRE}/{$MERCHANT_PUB}/{$H_CONTRACT_TERMS}/{$COIN_PUB}?merchant_sig={$merchant_sig}&timeout_ms={$timeout_ms}&lpt={$lpt}", $headers);
     }
 
     /**
