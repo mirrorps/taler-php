a219ac8b80fb7a8e2a60268f48d2ad0dbd3e034f
mirrorps <mirrorps.foss@gmail.com>
2025-06-06 17:40:25 +0300

    Refactor ResponseTest class

--- tests/Http/ResponseTest.php
+++ tests/Http/ResponseTest.php
@@ -2,12 +2,17 @@
 namespace Taler\Tests\Http;
 
 use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\MockObject\MockObject;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\StreamInterface;
 use Taler\Http\Response;
 
 class ResponseTest extends TestCase
 {
+    /**
+     * @param array<string, array<int, string>> $headers
+     * @return ResponseInterface
+     */
     private function createMockResponse(string $body = '{"foo":"bar"}', int $status = 200, array $headers = ['Content-Type' => ['application/json']]): ResponseInterface
     {
         $stream = $this->createMock(StreamInterface::class);
@@ -32,7 +37,10 @@ class ResponseTest extends TestCase
         return $response;
     }
 
-    public function test_it_decodes_json_body_to_data()
+    /**
+     * Test that JSON response body is decoded to data property
+     */
+    public function test_it_decodes_json_body_to_data(): void
     {
         $mockResponse = $this->createMockResponse('{"foo":"bar"}');
         $response = new Response($mockResponse);
@@ -41,7 +49,7 @@ class ResponseTest extends TestCase
         $this->assertEquals((object)['foo' => 'bar'], $response->data);
     }
 
-    public function test_it_returns_null_for_empty_body()
+    public function test_it_returns_null_for_empty_body():void
     {
         $mockResponse = $this->createMockResponse('');
         $response = new Response($mockResponse);
@@ -50,7 +58,7 @@ class ResponseTest extends TestCase
         $this->assertNull($response->data);
     }
 
-    public function test_it_delegates_getters_to_wrapped_response()
+    public function test_it_delegates_getters_to_wrapped_response():void
     {
         $mockResponse = $this->createMockResponse('{"foo":"bar"}', 201, [
             'Content-Type' => ['application/json'],
@@ -67,21 +75,45 @@ class ResponseTest extends TestCase
         $this->assertEquals('application/json', $response->getHeaderLine('Content-Type'));
     }
 
-    public function test_it_delegates_with_methods_to_wrapped_response()
+    public function test_it_delegates_with_methods_to_wrapped_response():void
     {
         $mockResponse = $this->createMockResponse();
         $response = new Response($mockResponse);
 
-        $this->assertSame($mockResponse, $response->withProtocolVersion('2.0'));
-        $this->assertSame($mockResponse, $response->withHeader('X-Test', 'val'));
-        $this->assertSame($mockResponse, $response->withAddedHeader('X-Test', 'val2'));
-        $this->assertSame($mockResponse, $response->withoutHeader('X-Test'));
-        $this->assertSame($mockResponse, $response->withBody($this->createMock(StreamInterface::class)));
-        $this->assertSame($mockResponse, $response->withStatus(404, 'Not Found'));
+        // Test immutability - each call should return a new instance
+        $newResponse1 = $response->withProtocolVersion('2.0');
+        $this->assertInstanceOf(Response::class, $newResponse1);
+        $this->assertNotSame($response, $newResponse1);
+
+        $newResponse2 = $response->withHeader('X-Test', 'val');
+        $this->assertInstanceOf(Response::class, $newResponse2);
+        $this->assertNotSame($response, $newResponse2);
+
+        $newResponse3 = $response->withAddedHeader('X-Test', 'val2');
+        $this->assertInstanceOf(Response::class, $newResponse3);
+        $this->assertNotSame($response, $newResponse3);
+
+        $newResponse4 = $response->withoutHeader('X-Test');
+        $this->assertInstanceOf(Response::class, $newResponse4);
+        $this->assertNotSame($response, $newResponse4);
+
+        /** @var StreamInterface&MockObject $mockStream */
+        $mockStream = $this->createMock(StreamInterface::class);
+        $newResponse5 = $response->withBody($mockStream);
+        $this->assertInstanceOf(Response::class, $newResponse5);
+        $this->assertNotSame($response, $newResponse5);
+
+        $newResponse6 = $response->withStatus(404, 'Not Found');
+        $this->assertInstanceOf(Response::class, $newResponse6);
+        $this->assertNotSame($response, $newResponse6);
+
+        // Verify that original response is unchanged
+        $this->assertEquals('1.1', $response->getProtocolVersion());
     }
 
-    public function test_get_body_returns_stream()
+    public function test_get_body_returns_stream():void
     {
+        /** @var StreamInterface&MockObject $stream */
         $stream = $this->createMock(StreamInterface::class);
         $mockResponse = $this->createMock(ResponseInterface::class);
         $mockResponse->method('getBody')->willReturn($stream);
