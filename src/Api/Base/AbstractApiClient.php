2dc2f5a5df8a29218cd2283c71f51f076be3193f
mirrorps <mirrorps.foss@gmail.com>
2025-06-06 20:06:04 +0300

    feat: introduce AbstractApiClient to reduce code duplication
    - Add AbstractApiClient as a new abstract class between BaseApiClass and the actual API clients
    - Move common client response logic to AbstractApiClient
    - Refactor ExchangeClient class to extend AbstractApiClient instead of BaseApiClass

---
+++ src/Api/Base/AbstractApiClient.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Taler\Api\Base;
+
+use Psr\Http\Message\ResponseInterface;
+
+abstract class AbstractApiClient extends BaseApiClient
+{
+    /**
+     * Handle response wrapping based on configuration
+     *
+     * @template T
+     * @param callable(ResponseInterface): T $handler The response handler function
+     * @return T|array<string, mixed>
+     */
+    protected function handleWrappedResponse(callable $handler): mixed
+    {
+        $response = $this->getResponse();
+
+        if ($this->getTaler()->getConfig()->getWrapResponse()) {
+            return $handler($response);
+        }
+
+        $decoded = json_decode((string) $response->getBody(), true);
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            throw new \RuntimeException('Invalid JSON response: ' . json_last_error_msg());
+        }
+
+        return $decoded;
+    }
+} 
