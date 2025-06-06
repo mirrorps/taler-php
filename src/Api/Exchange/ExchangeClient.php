feba99d34236bebc88579cacf58ff224c4c98ba9
s.stoev <miro@protexting.com>
2025-06-06 20:06:04 +0300

    feat: introduce AbstractApiClient to reduce code duplication
    - Add AbstractApiClient as a new abstract class between BaseApiClass and the actual API clients
    - Move common client response logic to AbstractApiClient
    - Refactor ExchangeClient class to extend AbstractApiClient instead of BaseApiClass

--- src/Api/Exchange/ExchangeClient.php
+++ src/Api/Exchange/ExchangeClient.php
@@ -3,7 +3,7 @@
 namespace Taler\Api\Exchange;
 
 use Psr\Http\Message\ResponseInterface;
-use Taler\Api\Base\BaseApiClient;
+use Taler\Api\Base\AbstractApiClient;
 use Taler\Api\Dto\ErrorDetail;
 use Taler\Api\Exchange\Dto\ExchangeVersionResponse;
 use Taler\Api\Exchange\Dto\TrackTransactionAcceptedResponse;
@@ -11,7 +11,7 @@ use Taler\Api\Exchange\Dto\TrackTransactionResponse;
 use Taler\Api\Exchange\Dto\TrackTransferResponse;
 use Taler\Exception\TalerException;
 
-class ExchangeClient extends BaseApiClient
+class ExchangeClient extends AbstractApiClient
 {
     /**
      * @param array<string, string> $headers Optional request headers
@@ -25,11 +25,7 @@ class ExchangeClient extends BaseApiClient
             $this->getClient()->request('GET', 'config', $headers)
         );
 
-        if (!$this->getTaler()->getConfig()->getWrapResponse()) {
-            return json_decode((string)$this->getResponse()->getBody(), true);
-        }
-
-        return $this->handleConfigResponse($this->getResponse());
+        return $this->handleWrappedResponse($this->handleConfigResponse(...));
     }
 
     /**
@@ -124,11 +120,7 @@ class ExchangeClient extends BaseApiClient
             $this->getClient()->request('GET', "transfers/{$wtid}", $headers)
         );
 
-        if (!$this->getTaler()->getConfig()->getWrapResponse()) {
-            return json_decode((string)$this->getResponse()->getBody(), true);
-        }
-
-        return $this->handleTransferResponse($this->getResponse());
+        return $this->handleWrappedResponse($this->handleTransferResponse(...));
     }
 
     /**
@@ -193,11 +185,7 @@ class ExchangeClient extends BaseApiClient
             $this->getClient()->request('GET', "deposits/{$H_WIRE}/{$MERCHANT_PUB}/{$H_CONTRACT_TERMS}/{$COIN_PUB}?merchant_sig={$merchant_sig}&timeout_ms={$timeout_ms}&lpt={$lpt}", $headers)
         );
         
-        if (!$this->getTaler()->getConfig()->getWrapResponse()) {
-            return json_decode((string)$this->getResponse()->getBody(), true);
-        }
-
-        return $this->handleDepositsResponse($this->getResponse());
+        return $this->handleWrappedResponse($this->handleDepositsResponse(...));
     }
 
     /**
