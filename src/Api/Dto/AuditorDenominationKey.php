d8c923087a06eee696c1edc5d71fd15cc3c75952
mirrorps <mirrorps.foss@gmail.com>
2025-06-09 22:45:00 +0300

    feat: add AuditorDenominationKey DTO and tests

---
+++ src/Api/Dto/AuditorDenominationKey.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Taler\Api\Dto;
+
+class AuditorDenominationKey
+{
+    /**
+     * Constructor for AuditorDenominationKey.
+     *
+     * @param string $denom_pub_h Hash of the public RSA key. Hash of the public RSA key used to sign coins of the respective denomination.
+     *                           Note that the auditor's signature covers more than just the hash, but this
+     *                           other information is already provided in denoms and thus not repeated here.
+     * @param string $auditor_sig Signature of TALER_ExchangeKeyValidityPS
+     */
+    public function __construct(
+        public readonly string $denom_pub_h, 
+        public readonly string $auditor_sig
+        )
+    {
+    }
+
+    /**
+     * Create an instance from an array.
+     *
+     * @param array<string, string> $data
+     * @return self
+     */
+    public static function fromArray(array $data): self
+    {
+        return new self(
+            $data['denom_pub_h'],
+            $data['auditor_sig']
+        );
+    }
+} 
---
+++ tests/Api/Dto/AuditorDenominationKeyTest.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Taler\Tests\Api\Dto;
+
+use PHPUnit\Framework\TestCase;
+use Taler\Api\Dto\AuditorDenominationKey;
+
+class AuditorDenominationKeyTest extends TestCase
+{
+    private const SAMPLE_DENOM_PUB_H = 'sample_denom_pub_h';
+    private const SAMPLE_AUDITOR_SIG = 'sample_auditor_sig';
+
+    public function testConstructorWithValidData(): void
+    {
+        $dto = new AuditorDenominationKey(self::SAMPLE_DENOM_PUB_H, self::SAMPLE_AUDITOR_SIG);
+
+        $this->assertEquals(self::SAMPLE_DENOM_PUB_H, $dto->denom_pub_h);
+        $this->assertEquals(self::SAMPLE_AUDITOR_SIG, $dto->auditor_sig);
+    }
+
+    public function testFromArrayWithValidData(): void
+    {
+        $data = [
+            'denom_pub_h' => self::SAMPLE_DENOM_PUB_H,
+            'auditor_sig' => self::SAMPLE_AUDITOR_SIG,
+        ];
+
+        $dto = AuditorDenominationKey::fromArray($data);
+
+        $this->assertEquals(self::SAMPLE_DENOM_PUB_H, $dto->denom_pub_h);
+        $this->assertEquals(self::SAMPLE_AUDITOR_SIG, $dto->auditor_sig);
+    }
+} 
