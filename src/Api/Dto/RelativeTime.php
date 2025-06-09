2cb8013aed618d5448e4b55fba4a6e4f2fc276e1
mirrorps <mirrorps.foss@gmail.com>
2025-06-09 17:45:03 +0300

    feat: add RelativeTime DTO and the corresponding unit tests
    - Introduce RelativeTime DTO for type-safe data transfer
    - Implement RelativeTimeTest.php to validate the DTO's properties and constructor functionality

---
+++ src/Api/Dto/RelativeTime.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Taler\Api\Dto;
+
+/**
+ * DTO for relative time duration
+ * 
+ * @see https://docs.taler.net/core/api-common.html
+ */
+class RelativeTime
+{
+    private const MAX_MICROSECONDS = 9007199254740991; // 2^53 - 1
+
+    /**
+     * @param int|string $d_us Duration in microseconds or "forever" to represent an infinite duration
+     */
+    public function __construct(
+        public readonly int|string $d_us,
+    ) {
+        if (is_int($d_us) && ($d_us < 0 || $d_us > self::MAX_MICROSECONDS)) {
+            throw new \InvalidArgumentException(
+                'Duration must be between 0 and ' . self::MAX_MICROSECONDS . ' microseconds or "forever"'
+            );
+        }
+        if (is_string($d_us) && $d_us !== 'forever') {
+            throw new \InvalidArgumentException('String duration can only be "forever"');
+        }
+    }
+
+    /**
+     * Creates a new instance from an array of data
+     *
+     * @param array{d_us: int|string} $data
+     */
+    public static function fromArray(array $data): self
+    {
+        return new self(
+            d_us: $data['d_us']
+        );
+    }
+} 
