3cf0ef8cac49a5732f57d5334097c0f00a8b80c7
mirrorps <mirrorps.foss@gmail.com>
2025-06-09 22:31:34 +0300

    feat: add Recoup DTO and tests

---
+++ src/Api/Dto/Recoup.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Taler\Api\Dto;
+
+/**
+ * DTO for denomination revocation under emergency protocol
+ * 
+ * @see https://docs.taler.net/core/api-exchange.html
+ */
+class Recoup
+{
+    /**
+     * @param string $h_denom_pub Hash of the public key of the denomination that is being revoked under
+     *                           emergency protocol (see /recoup).
+     */
+    public function __construct(
+        public readonly string $h_denom_pub,
+    ) {
+    }
+
+    /**
+     * Creates a new instance from an array of data
+     *
+     * @param array{h_denom_pub: string} $data
+     */
+    public static function fromArray(array $data): self
+    {
+        return new self(
+            h_denom_pub: $data['h_denom_pub']
+        );
+    }
+} 
