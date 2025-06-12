babd03b291250147f4f360b6cfc114c03a695346
s.stoev <miro@protexting.com>
2025-06-12 18:52:59 +0300

    refactor classes DenomGroupRsa, DenomGroupRsaTest to work with timestamps from array and DenomCommon DTO

--- src/Api/Dto/DenomGroupRsa.php
+++ src/Api/Dto/DenomGroupRsa.php
@@ -15,15 +15,7 @@ class DenomGroupRsa extends AbstractDenomGroup
      * @param string $fee_deposit Fee charged by the exchange for depositing a coin of this denomination
      * @param string $fee_refresh Fee charged by the exchange for refreshing a coin of this denomination
      * @param string $fee_refund Fee charged by the exchange for refunding a coin of this denomination
-     * @param array<int, array{
-     *     master_sig: string,
-     *     stamp_start: array{t_s: int|string},
-     *     stamp_expire_withdraw: array{t_s: int|string},
-     *     stamp_expire_deposit: array{t_s: int|string},
-     *     stamp_expire_legal: array{t_s: int|string},
-     *     rsa_pub: string,
-     *     lost?: bool
-     * }> $denoms Array of denomination details with RSA public keys
+     * @param array<int, DenomCommon> $denoms Array of denomination details
      */
     public function __construct(
         string $value,
@@ -31,14 +23,25 @@ class DenomGroupRsa extends AbstractDenomGroup
         string $fee_deposit,
         string $fee_refresh,
         string $fee_refund,
-        array $denoms,
+        protected readonly array $denoms,
     ) {
-        parent::__construct($value, $fee_withdraw, $fee_deposit, $fee_refresh, $fee_refund, $denoms);
+        parent::__construct(
+            $value,
+            $fee_withdraw,
+            $fee_deposit,
+            $fee_refresh,
+            $fee_refund
+        );
     }
 
     public function getCipher(): string
     {
         return self::CIPHER;
+    }
+
+    public function getDenoms(): array
+    {
+        return $this->denoms;
     }
 
     /**
@@ -78,7 +81,10 @@ class DenomGroupRsa extends AbstractDenomGroup
             fee_deposit: $data['fee_deposit'],
             fee_refresh: $data['fee_refresh'],
             fee_refund: $data['fee_refund'],
-            denoms: $data['denoms']
+            denoms: array_map(
+        fn(array $denom) => DenomCommon::fromArray($denom),
+                $data['denoms']
+            ),
         );
     }
-} 
+}
